262f49189a401a05a35e823138cf311ef9770947
nickridwan <ridwanhanif165@gmail.com>
2022-05-30 18:44:04 +0700

    second commit

---
+++ script..js
@@ -0,0 +1,9 @@
+const jumlahAngkot = 10;
+
+for (let i = 1; i <= jumlahAngkot; i++) {
+  if (i % 2 == 1) {
+    console.log("angkot no." + i + " ber operasi dengan baik");
+  } else {
+    console.log("Angkot no." + i + " tidak ber operasi dengan baik");
+  }
+}
